d42c26004eb049477c7ad71a39c5dfe4fff9d5d3
Supraj SS <suprajsseeryada2003@gmail.com>
2022-11-11 21:15:09 +0530

    Quiz Game

---
+++ finding no of seconds.py
@@ -0,0 +1,12 @@
+#finding number of seconds in a year
+seconds_day = 60*60*24
+print(seconds_day)
+
+#number of seconds in a week
+seconds_week = seconds_day*7
+print(seconds_week)
+
+#number of seconds in a month
+seconds_month = seconds_week*30
+print(seconds_month)
+
---
+++ main.py
@@ -0,0 +1,34 @@
+print('Hello world')
+print("supraj cinematography")
+
+#declaring a single integer variable
+integervalue = 10
+print(integervalue)
+
+#declaring a float variable
+floatvalue = 12.3
+print(floatvalue)
+
+#declaring the string variable
+stringvalue = 'great person'
+print(stringvalue)
+stringvalue = '20'
+print(stringvalue)
+
+#declaring a single boolian value
+boolianvalue = True #T must be always capital
+print(boolianvalue)
+
+#declaring a complex variable
+print(type(integervalue))
+
+#declarin multiple variables in a single line
+integervalue, floatvalue = 10, 10.5
+stringvalue, boolianvalue = 'hello man', True
+print(integervalue, floatvalue, stringvalue, boolianvalue)
+
+#using semicolon
+integervalue = 14; print(integervalue) #semicolin acts as goint to next line without enter
+integervalue = 'suppi'; print(integervalue) #we can give or assign anything for integervalue or also for loat and all
+
+
